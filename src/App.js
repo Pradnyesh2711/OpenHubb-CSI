ef6945ea2c99f02850798dc2a56ff25e94a2685a
Darshini2610 <kadmedarshini@gmail.com>
2024-02-10 22:56:36 +0530

    api sorted

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import LandingPage from './Pages/LandingPage/LandingPage';
 import Navigation from './Components/Navigation';
 import Contribute from './Pages/Contribute/Contribute';
 import HomePage from './Pages/HomePage/HomePage';
+import Profile from './Pages/Profile/Profile';
+import Issues from './Pages/Issues/Issues';
 
 function App() {
 const[isLogin,setLogin]=useState(false)
@@ -18,6 +20,8 @@ const[isLogin,setLogin]=useState(false)
           <Route path='/' Component={LandingPage}/>
           <Route path='/home' Component={HomePage}/>
           <Route path='/contribute' Component={Contribute}/>
+          <Route path='/profile' Component={Profile}/>
+          <Route path='/issues' Component={Issues}/>
         </Routes>
       </BrowserRouter>
     </div>
